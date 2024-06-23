51d12a4e2d481d94da42ea44c0d66770cf252403
muhammad-nomankd <nomandurrani29@gmail.com>
2024-06-23 23:10:13 +0500

    navigation bug

--- app/src/main/java/com/example/alarmmanager/activities/MainActivity.kt
+++ app/src/main/java/com/example/alarmmanager/activities/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.alarmmanager.activities
 
 import CreateTaskViewModel
+import SignUp
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -30,22 +32,34 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.alarmmanager.repositories.AuthRepository
 import com.example.alarmmanager.screens.CreatTask
 import com.example.alarmmanager.screens.HomeScreen
 import com.example.alarmmanager.screens.TaskDetailScreen
 import com.example.alarmmanager.ui.theme.AlarmManagerTheme
+import com.example.alarmmanager.viewmodels.AuthViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
     var isloading= mutableStateOf(false)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val googleSignInLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                AuthViewModel(AuthRepository()).handleGoogleSignInResult(result.data, this)
+
+            }
 
 
         setContent {
             val navController = rememberNavController()
             NavHost(navController = navController, startDestination = "home") {
                 composable("home") { HomeScreen().HomeScreenUi(navController, this@MainActivity) }
+                composable("signup") { SignUp(
+                    viewModel = AuthViewModel(AuthRepository()),
+                    navController = navController,
+                    googleSignInLauncher = googleSignInLauncher
+                ) }
                 composable("createTask") { CreatTask().createTaskcom(navController, CreateTaskViewModel()) }
                 composable("taskListScreen"){ TaskDetailScreen().TaskList()}
             }
